58147d2a4a187d258ee9ca4c2bae921f4be76810
OldSinner <antylifepatia@gmail.com>
2020-12-03 22:32:27 +0100

    Loading Units from Api

--- BlazorBattle/Client/Services/UnitService.cs
+++ BlazorBattle/Client/Services/UnitService.cs
@@ -3,6 +3,8 @@ using Blazored.Toast.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace BlazorBattle.Client.Services
@@ -10,17 +12,15 @@ namespace BlazorBattle.Client.Services
     public class UnitService : IUnitService
     {
         private readonly IToastService _toastService;
+        private readonly HttpClient _http;
 
-        public UnitService(IToastService toastService)
+        public UnitService(IToastService toastService, HttpClient http)
         {
             _toastService = toastService;
+            _http = http;
         }
-        public IList<Unit> Units { get; } = new List<Unit>
-        {
-            new Unit {id=1,name="Knight",Attack=10,Defense=10, BananaCost=100 },
-            new Unit {id=2,name="Archer",Attack=15,Defense=5, BananaCost=120 },
-            new Unit {id=3,name="Mage",Attack=20,Defense=1, BananaCost=150 }
-        };
+        public IList<Unit> Units { get; set; } = new List<Unit>();
+        
 
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
 
@@ -30,6 +30,14 @@ namespace BlazorBattle.Client.Services
             MyUnits.Add(new UserUnit { UnitId = unit.id, HitPoints = unit.HitPoints });
             _toastService.ShowSuccess($"Your {unit.name} has been built!", "Unit Built!");
 
+        }
+
+        public async Task LoadUnistAsync()
+        {
+            if(Units.Count==0)
+            {
+                Units = await _http.GetFromJsonAsync<IList<Unit>>("api/unit");
+            }
         }
     }
 }
