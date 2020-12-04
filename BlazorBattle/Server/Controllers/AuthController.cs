4c55de009e28e66817af8292d7d877c0c1f376ca
OldSinner <antylifepatia@gmail.com>
2020-12-04 11:59:50 +0100

    Add Register API

---
+++ BlazorBattle/Server/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using BlazorBattle.Server.Data;
+using BlazorBattle.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattle.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegister request)
+        {
+            var response = await _authRepository.Register(
+                new User
+                {
+                    Username = request.Username,
+                    Email = request.Email,
+                    Bananas = request.Bananas,
+                    DateOfBirth = request.DateOfBirth,
+                    isConfirmed = request.isConfirmed,
+                   
+                }, request.Password
+                );
+            if(!response.isSuccess)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
