b090c8cd5713398813af48c9acd3b31d678b277c
OldSinner <antylifepatia@gmail.com>
2020-12-04 12:32:06 +0100

    Create Register connter to API and DB

---
+++ BlazorBattle/Client/Services/AuthService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorBattle.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorBattle.Client.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _http;
+
+        public AuthService(HttpClient http)
+        {
+            _http = http;
+        }
+        public async Task<ServiceResponse<int>> Regsiter(UserRegister request)
+        {
+            var result = await _http.PostAsJsonAsync("api/Auth/register", request);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+        }
+    }
+}
